9bb5596c47c8f28cc61d51268b9334e83a9d6919
straus <straus94@gmail.com>
2020-02-18 00:33:06 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.js-slider').slick({
+        // slidesToShow: 1,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        // arrow: true
+    });
+});
